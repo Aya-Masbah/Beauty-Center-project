a030a7a24884eb5f8cb560bd5d4898608a616882
Masbah Aya <aya.masbah1729@gmail.com>
2024-01-26 12:46:09 +0100

    logo

--- beauty-center-new2/resources/views/user/home.blade.php
+++ beauty-center-new2/resources/views/user/home.blade.php
@@ -20,6 +20,18 @@
     <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
 
     <link rel="stylesheet" href="../assets/css/theme.css">
+
+    <style>
+        .navbar-brand img {
+            width: 200px;
+            height: 200px;
+            position: absolute;
+            left: -1px;
+            /* Adjust this value to position the logo */
+            top: -40px;
+            /* Adjust this value to position the logo */
+        }
+    </style>
 </head>
 
 <body>
@@ -28,30 +40,11 @@
     <div class="back-to-top"></div>
 
     <header>
-        <div class="topbar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-8 text-sm">
-                        <div class="site-info">
-                            <a href="#"><span class="mai-call text-primary"></span> +00 123 4455 6666</a>
-                            <span class="divider">|</span>
-                            <a href="#"><span class="mai-mail text-primary"></span> JessicaBeauty@example.com</a>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 text-right text-sm">
-                        <div class="social-mini-button">
-                            <a href="#"><span class="mai-logo-facebook-f"></span></a>
-                            <a href="#"><span class="mai-logo-twitter"></span></a>
-                            <a href="#"><span class="mai-logo-dribbble"></span></a>
-                            <a href="#"><span class="mai-logo-instagram"></span></a>
-                        </div>
-                    </div>
-                </div> <!-- .row -->
-            </div> <!-- .container -->
-        </div> <!-- .topbar -->
-
         <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
             <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/img/logo.png" alt="Bella Jessica Logo">
+                </a>
                 <a class="navbar-brand" href="#"><span class="text-primary">Bella Jessica</span>-Beauty Lounge</a>
 
                 <form action="#">
